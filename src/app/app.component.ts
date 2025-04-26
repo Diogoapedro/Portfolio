7cdd1916a6040b45283218ee68dc9672c3e8e2a7
Diogoapedro <diogoapedro02@gmail.com>
2025-04-26 19:21:42 +0100

    Setups, bootstrap, angular material and mentioned components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FootbarComponent } from "./components/footbar/footbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FootbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
