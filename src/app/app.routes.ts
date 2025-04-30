70a4109c4b2bf240a4793443835064a61d39f81e
Diogoapedro <diogoapedro02@gmail.com>
2025-04-30 16:13:24 +0100

    New component added and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomePageComponent },
+    //{ path: 'team', component: TeamPageComponent },
+    //{ path: 'membership', component: MembershipPageComponent },
+    //{ path: 'contact-us', component:  ContactUsPageComponent},
+    //{ path: 'documents', component: DocumentsPageComponent }, 
+];
